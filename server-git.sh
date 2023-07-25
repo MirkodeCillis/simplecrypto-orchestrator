61632bcfc1394751f164814082ff3f02451d5374
Mirko de Cillis <43855931+MirkodeCillis@users.noreply.github.com>
2023-07-25 09:14:01 +0200

    Update server-git.sh

--- server-git.sh
+++ server-git.sh
@@ -1,1 +1,1 @@
-ssh-agent bash -c 'ssh-add ~/.ssh/sc-server; git clone git@github.com:MirkodeCillis/simplecrypto-server.git'
+ssh-agent bash -c 'ssh-add ~/.ssh/sc-server; git clone git@github.com:MirkodeCillis/simplecrypto-backend.git'
